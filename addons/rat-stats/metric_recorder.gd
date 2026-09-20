f8aa8643a1e0ce1f9b47c24adae0e0ab1146808e
Haiden <46227616+hrothwell@users.noreply.github.com>
2026-09-20 15:33:56 -0700

    update recorder

--- addons/rat-stats/metric_recorder.gd
+++ addons/rat-stats/metric_recorder.gd
@@ -1,53 +1,73 @@
 ## Maintains a log of metric points emitted by MetricMonitor instances.
+## Logs are exported as <log_folder>/rat-stats/<utc timestamp at export time>.json.
 class_name MetricRecorder extends Node
 
-## Monitors that will be recorded in _metric_logs
+## Initial monitors that will be recorded in _metric_logs.
 ## Additional monitors can be recorded via record_monitor but will not be added here. If
-## all currently recording monitors are needed use <>
+## all currently recording monitors are needed use currently_recording.
 @export
 var _initial_monitors_to_record: Array[MetricMonitor] = []
 
-## MetricMonitors that are currently being recorded
+## MetricMonitors that are currently being recorded.
 var currently_recording: Array[MetricMonitor] = []
 
-## If not present no logs will be exported, must manually call export_logs
+## Export logs on timer timeout. 
+## If not present no logs will be exported unless export_logs is called manually.
 @export 
-var dump_logs_timer: Timer
+var export_logs_timer: Timer
 
+## Root folder to export logs.
 @export
 var log_folder: String = "user://logs/"
 
+## Calculated log location. <log_folder>/rat-stats/
+var _folder: String
+
+## Entries contain: { utc_timestamp: String, metric: String, value: Variant }. 
 var _metric_logs: Array[Dictionary]
 
 func _ready() -> void:
+	_folder = str(log_folder, "rat-stats/")
+	if !DirAccess.dir_exists_absolute(_folder):
+		DirAccess.make_dir_recursive_absolute(_folder)
+	
 	for m in _initial_monitors_to_record:
 		record_monitor(m)
 	
-	if dump_logs_timer:
-		dump_logs_timer.timeout.connect(export_logs)
+	if export_logs_timer:
+		export_logs_timer.timeout.connect(export_logs)
 
+## Start recording a new montior. Upon exiting tree this monitor will be removed from currently_recording.
 func record_monitor(monitor: MetricMonitor) -> void:
-	monitor.value_updated.connect(func(v: Variant): _log_metric(monitor.get_metric_name(), v))
-	currently_recording.append(monitor)
+	monitor.value_updated.connect(log_metric.bind(monitor.get_metric_name()))
 	monitor.tree_exiting.connect(remove_monitor.bind(monitor))
 	
+	if !currently_recording.has(monitor):
+		currently_recording.append(monitor)
+	
+## Stop recording monitor.
 func remove_monitor(monitor: MetricMonitor) -> void:
+	monitor.value_updated.disconnect(log_metric)
+	monitor.tree_exiting.disconnect(remove_monitor)
+	
 	var index := currently_recording.find(monitor)
 	if index > -1:
 		currently_recording.remove_at(index)
 
-func _log_metric(name: String, value: Variant) -> void:
+## Record a new metric point.
+func log_metric(value: Variant, name: String) -> void:
 	_metric_logs.append(
 		{
-			"timestamp": Time.get_datetime_string_from_system(),
-			 "name": name,
+			"utc_timestamp": Time.get_datetime_string_from_system(true),
+			 "metric": name,
 			 "value": value
 		}
 	)
 
+## Export the logs to configured folder. _metric_logs are cleared. 
 func export_logs() -> void:
-	var current_time := Time.get_datetime_dict_from_system()
-	var date_string: String = str(current_time.get("year"), current_time.get("month"), current_time.get("day"), current_time.get("hour"), current_time.get("minute"), current_time.get("second"))
-	var filepath: String = str(log_folder, "rat-stats-", date_string, ".json")
+	var date_string := Time.get_datetime_string_from_system(true).replace(":", ".")
+	var filepath: String = str(_folder, date_string, "Z", ".json")
 	var file = FileAccess.open(filepath, FileAccess.WRITE)
 	file.store_line(JSON.stringify(_metric_logs))
+	_metric_logs.clear()
